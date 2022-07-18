5400949cdfc86c6394af3589b1212033c5ced55e
Anil5520 <anilmahto812@gmail.com>
2022-07-18 16:00:46 +0530

    phase1 complete

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,4 +5,8 @@ const urlController=require('../controller/urlController')
 router.post('/url/shorten', urlController.createShortenURL)
 router.get('/:urlCode', urlController.getUrlByUrlCode)
 
+router.all('/**', function(req, res){
+    res.status(400).send({status: false, messsage:"invalid http request"})
+})
+
 module.exports=router
