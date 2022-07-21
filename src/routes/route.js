dc4d2449fa793734d82cdac7d7b524530b25ff8b
Anand Sharma <sharmaanand570@gmail.com>
2022-07-21 13:29:16 +0530

    Update

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,16 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const urlController = require('../controller/urlController')
-<<<<<<< HEAD
 
 //-----------------------------------------------------------------------------------//
-=======
->>>>>>> f2399d0142401229f6b5260056ed065601db5f1e
 
 router.post('/url/shorten', urlController.createShortenURL)
 router.get('/:urlCode', urlController.getUrlByUrlCode)
 
-<<<<<<< HEAD
 router.all("/**", function (req, res) {
     res.status(404).send({ status: false, message: "The api you request is not available" })
 })
@@ -20,10 +16,3 @@ router.all("/**", function (req, res) {
 module.exports = router
 
 //-----------------------------------------------------------------------------------//
-=======
-router.all('/**', function (req, res) {
-    res.status(400).send({ status: false, messsage: "invalid http request" })
-})
-
-module.exports = router
->>>>>>> f2399d0142401229f6b5260056ed065601db5f1e
